855851bc9c9a6f81782d16065538fea0475097d5
elisasto <elisasto@student.hist.no>
2016-10-18 12:08:31 +0200

    list in eventlist

--- app/src/main/java/com/example/andreasbergman/appadmin2/EventListActivity.java
+++ app/src/main/java/com/example/andreasbergman/appadmin2/EventListActivity.java
@@ -1,25 +1,30 @@
 package com.example.andreasbergman.appadmin2;
 
+import android.content.res.Resources;
 import android.net.Uri;
+import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.appindexing.Thing;
 import com.google.android.gms.common.api.GoogleApiClient;
 
+import static com.example.andreasbergman.appadmin2.R.id.listview1;
+
 public class EventListActivity extends AppCompatActivity {
 
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
     private Toolbar mToolbar;
+    private ListView eventlist;
+    private String[] events;
     /**
      * ATTENTION: This was auto-generated to implement the App Indexing API.
      * See https://g.co/AppIndexing/AndroidStudio for more information.
@@ -45,6 +50,11 @@ public class EventListActivity extends AppCompatActivity {
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
         client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+        Resources resources = getResources();
+       events = resources.getStringArray(R.array.event); //test list
+        eventlist = (ListView)findViewById(listview1);  // Get ListView object from xml
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, events); // Define a new Adapter
+        eventlist.setAdapter(adapter);     // Assign adapter to ListView
     }
 
     @Override
