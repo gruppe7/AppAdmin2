3975017227e23de3bcdd4ae4ad86f4485a08f35f
elisasto <elisasto@student.hist.no>
2016-10-25 13:33:01 +0200

    Event View
    Førsteutkast event view

--- app/src/main/java/com/example/andreasbergman/appadmin2/EventActivity.java
+++ app/src/main/java/com/example/andreasbergman/appadmin2/EventActivity.java
@@ -1,7 +1,10 @@
 package com.example.andreasbergman.appadmin2;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class EventActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class EventActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event);
+        TextView back = (TextView)findViewById(R.id.backLinkTV);//temporarily for testing
+        back.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(EventActivity.this, EventListActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
